510013cad734d40c4d62c8401fa8b4dfbacba37b
MRSS <mateusyasd@hotmail.com>
2022-09-10 00:57:44 -0300

    moved api urls to .env

--- src/request/request.service.ts
+++ src/request/request.service.ts
@@ -16,24 +16,24 @@ export class RequestService {
  private readonly logger = new Logger(RequestService.name);
   
  //@Cron('0 30 11 * * *')
- @Cron('2 * * * * *')
+ @Cron('02 * * * * *')
  async scheduledRequest(): Promise<any> {
     this.logger.debug('Called when the current second is 2');
     const parser = new Parser();
-    const usUrl = 'https://disease.sh/v3/covid-19/countries/usa';
+    const usUrl = process.env.US_URL;
     const dataUS = await this.requestService.getInfo(usUrl);
     const csvUs = parser.parse(dataUS);
-    const chinaUrl = 'https://disease.sh/v3/covid-19/countries/china';
+    const chinaUrl = process.env.CHINA_URL;
     const dataChina = await this.requestService.getInfo(chinaUrl);
     const csvChina = parser.parse(dataChina);
     try {
     fs.writeFile('../../files/china.csv', csvChina, async () => { 
       this.logger.debug('china.csv created!');
-      await this.upload('./china.csv'); 
+      await this.upload('../../files/china.csv'); 
     })
     fs.writeFile('../../files/us.csv', csvUs, async () => { 
       this.logger.debug('us.csv created!');
-      await this.upload('./us.csv'); 
+      await this.upload('../../files/us.csv'); 
     })
     } catch (e) {
       throw e;
